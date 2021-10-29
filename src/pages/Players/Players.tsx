0b61937473c09c57d5ee7140128c90208b1e9926
Imac <imac@iMac-de-Imac.local>
2021-10-29 16:19:10 -0500

    [UPDATE] menor fixes

--- src/pages/Players/Players.tsx
+++ src/pages/Players/Players.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { ErrorPage, PlayersList } from '../../components';
+import { FaUserPlus } from 'react-icons/fa';
+import { useHistory } from 'react-router-dom';
+
+import { Button, ErrorPage, PlayersList } from '../../components';
 import { usePlayers } from '../../hooks';
 
+import greekFreak from '../../assets/svg/greek_freak.svg';
+
 const Players: React.FC = () => {
+  const history = useHistory();
   const { data, error, loading } = usePlayers();
 
   if (loading) {
@@ -24,6 +30,18 @@ const Players: React.FC = () => {
   return(
     <div className="w-full">
       <div className="max-w-4xl mx-auto">
+        {!Boolean(data.length) && (
+          <div className="w-full p-10 rounded-lg bg-white flex flex-col items-center justify-center">
+            <img className="w-1/3" src={greekFreak} alt='empty' />
+            <h2 className="my-5 text-2xl font-semibold text-gray-600">No hay jugadores en el sistema</h2>
+            <Button
+              onClick={() => history.push('/players/new')}
+              icon={FaUserPlus}
+            >
+              Agregar jugador
+            </Button>
+          </div>
+        )}
         <PlayersList players={data} />
       </div>
     </div>
